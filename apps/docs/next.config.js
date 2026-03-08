2eb013e51ce80bb80226fdcef5177fdecf67b553
Piyush Zingade <piyushzingade@gmail.com>
2026-03-08 18:48:00 +0530

    feat(docs): initialize documentation site with fumadocs

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,9 @@
+import withMotionwind from "motionwind/next";
+import { createMDX } from "fumadocs-mdx/next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const withMDX = createMDX();
+
+export default withMDX(withMotionwind(nextConfig));
